966a364714acac8f0ae0458c769c639fe4341387
IgorKhnykin <igor.khnykin@mail.ru>
2025-06-06 13:07:45 +0300

    added proto file

--- rangiffler-core/src/main/java/guru/ga/rangiffler/service/UserService.java
+++ rangiffler-core/src/main/java/guru/ga/rangiffler/service/UserService.java
@@ -2,8 +2,9 @@ package guru.ga.rangiffler.service;
 
 import guru.ga.rangiffler.data.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.grpc.server.service.GrpcService;
 
-
+@GrpcService
 public class UserService {
 
     private final UserRepository userRepository;
@@ -12,4 +13,6 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+
 }
